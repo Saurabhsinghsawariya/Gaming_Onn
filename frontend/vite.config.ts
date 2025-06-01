8ff4f658dd068e05d03f1270325251b4b816ef1e
Saurabhsinghsawariya <97609934+Saurabhsinghsawariya@users.noreply.github.com>
2025-06-01 10:55:47 +0530

    5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // output folder named 'dist'
+    outDir: '.dist'
   }
-})
+});
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
