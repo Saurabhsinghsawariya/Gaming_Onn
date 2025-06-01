c2745eee2d85a08391f69c3ac2cad9788c47ebf0
Saurabhsinghsawariya <97609934+Saurabhsinghsawariya@users.noreply.github.com>
2025-06-01 08:47:53 +0530

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3000/api/users/login', {
+      const response = await fetch('https://gaming-onn.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
